11f399640e8a4dc2e6a1140c203ffb4a6ab8acf4
pankajpal05 <pankaj.pal@ayasya.com>
2025-03-25 19:08:42 +0530

    update homepage

--- src/service/homePage.service.js
+++ src/service/homePage.service.js
@@ -1,7 +1,7 @@
 const fetchHomePageData = async () => {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/home?populate[heroBanner][populate]=*&populate[latestSection][populate]=newsTeaser.image&populate[latestSection][populate]=socialTeaser.image&populate[legacyTeaser][populate]%3D=*&populate[achievementSection][populate]=*&populate[brandSection][populate]=*&populate[carrersSection][populate]=carrerTeaser.image&populate[carrersSection][populate]=slider`
+      `${process.env.NEXT_API_URL}/home?populate[heroBanner][populate]=*&populate[latestSection][populate]=newsTeaser.image&populate[latestSection][populate]=socialTeaser.image&populate[legacyTeaser][populate]%3D=*&populate[achievementSection][populate]=*&populate[brandSection][populate]=*&populate[carrersSection][populate]=carrerTeaser.image&populate[carrersSection][populate]=slider`
     );
     const responseBody = await response.json();
     return responseBody?.data;
